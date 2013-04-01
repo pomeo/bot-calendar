7522aaf2e3d395ce487f98c3c43f9557df376ac5
Sergey Ovechkin <me@sovechkin.com>
2013-04-01 16:31:59 +0400

    delete auth function

--- app.js
+++ app.js
@@ -98,10 +98,3 @@ xmpp.connect({
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-function ensureAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect('/login');
-}
